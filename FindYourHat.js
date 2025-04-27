a5c0a22d981c0186cf88f8e5a1a6d3a5aee1fa22
Jonathan Rider <jonprider8@gmail.com>
2025-04-27 19:53:32 -0400

    Fixed border control and handling bad input
    Players now hitting the border will no longer crash the game and be prompted instead. Also handled bad input.

--- FindYourHat.js
+++ FindYourHat.js
@@ -10,6 +10,8 @@ class Field {
     constructor(field) {
         this._field = field;
         this._playerPosition = { x: 0, y: 0 };
+        this._border = false;
+        this._badInput = false;
     }
 
     get field(){
@@ -20,68 +22,116 @@ class Field {
         return this._playerPosition;
     }
 
+    get border(){
+        return this._border;
+    }
+
+    set border(newBorder){
+        this._border = newBorder;
+    }
+
+    get badInput(){
+        return this._badInput;
+    }
+
+    set badInput(newBadInput){
+        this._badInput = newBadInput;
+    }
+
     //Method that changes player position in the field and reprints
     set playerPosition(newPosition){
         let preposition = this.playerPosition;
-        let border = false;
+        
         var fieldLength = 0;
         var fieldWidth = 0;
 
-        for(let arr in this._field){
+        for(let arr in this.field){
             fieldLength++;
         }
-        for(let arr in this._field[0]){
+        for(let arr in this.field[0]){
             fieldWidth++;
         }
 
-        if(newPosition === 'w'){
-            this.playerPosition.y -= 1;
-        }else if(newPosition === 's'){
-            this.playerPosition.y += 1;
-        } else if(newPosition === 'a'){
-            this.playerPosition.x -= 1;
-        } else if(newPosition === 'd'){
-            this.playerPosition.x += 1;
-        }else if(newPosition === 'e'){
-            console.log("You have exited the game.");
+        //Exit game
+        if(newPosition === 'e'){
+            console.log("\nYou have exited the game.");
             process.exit();
         }
 
-        if(this.field[this.playerPosition.y] === undefined || this.field[this.playerPosition.x] === undefined){
-            this.playerPosition = preposition;
-            border = true;
+        //changes player position in the field
+        switch(newPosition){
+            case 'w':
+                preposition.y -= 1;
+                break;
+            case 's':
+                preposition.y += 1;
+                break;
+            case 'a':
+                preposition.x -= 1;
+                break;
+            case 'd':
+                preposition.x += 1;
+                break;
+            default:
+                this.badInput = true;
+
+        }
+
+        //checks if player is out of bounds
+        if(this.field[preposition.y] === undefined || this.field[preposition.x] === undefined){
+            this.border = true;
+
+            switch(newPosition){
+                case 'w':
+                    preposition.y += 1;
+                    break;
+                case 's':
+                    preposition.y -= 1;
+                    break;
+                case 'a':
+                    preposition.x += 1;
+                    break;
+                case 'd':
+                    preposition.x -= 1;
+                    break;
+            }
+
         }else{
+
             this.hatOrHole();
+
+            //update player position
             this.field[this.playerPosition.y][this.playerPosition.x] = pathCharacter;
-            border = false;
         }
         
-        console.clear();
-        this.field.forEach(row => {
-            console.log(row.join(' '));
-        });
-        if(border === true){
-            console.log("You have hit the wall!");
-        }
-        console.log("What way do you want to go? (E to Exit)> ")
+        this.print()
+        
     }
 
+    //checks if player has found the hat or fell into a hole
     hatOrHole(){
-        if(this.field[this.playerPosition.y] === hat || this.field[this.playerPosition.x] === hat){
+        if(this.field[this.playerPosition.y][this.playerPosition.x] === hat){
             console.log("You found the hat! You win!");
             process.exit();
-        } else if(this.field[this.playerPosition.y] === hole || this.field[this.playerPosition.x] === hole){
+        } else if(this.field[this.playerPosition.y][this.playerPosition.x] === hole){
             console.log("You fell into a hole! You lose!");
             process.exit();
         }
     }
 
-
+    //prints the field with player position
     print() {
         console.clear();
-        this._field.forEach(row => {
+        this.field.forEach(row => {
             console.log(row.join(' '));
         });
+        if(this.border === true){
+            console.log("You have hit the wall!");
+            this.border = false;
+        } else if(this.badInput === true){
+            console.log("Invalid input! Please try again.");
+            this.badInput = false;
+        }
         console.log("What way do you want to go? (E to Exit)> ")
     }
 }
